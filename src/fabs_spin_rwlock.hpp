8d15d9523c3f0aa728e24e8bb39cb865e152eeb9
Yuuki Takano <ytakanoster@gmail.com>
2016-11-26 16:18:13 +0900

    use spin rwlock

--- src/fabs_spin_rwlock.hpp
+++ src/fabs_spin_rwlock.hpp
@@ -1,6 +1,8 @@
 #ifndef FABS_SPIN_RWLOCK_HPP
 #define FABS_SPIN_RWLOCK_HPP
 
+#include <pthread.h>
+
 #if defined(__x86_64__) || defined(__i686__)
   #include <xmmintrin.h>
   #define _MM_PAUSE _mm_pause()
@@ -8,25 +10,29 @@
   #define _MM_PAUSE
 #endif // __x86_64__ || __i386__
 
-class spin_lock_read;
-class spin_lock_write;
+class fabs_spin_rwlock_read;
+class fabs_spin_rwlock_write;
 
-class spin_rwlock {
+class fabs_spin_rwlock {
 public:
-    spin_rwlock() : m_read_count(0), m_write_count(0), m_is_writing(0) { }
+    fabs_spin_rwlock() : m_read_count(0), m_write_count(0), m_is_writing(0)
+    {
+        pthread_mutex_init(&m_write_mutex, nullptr);
+    }
 
 private:
-    volatile int m_read_count;
-    volatile int m_write_count;
-    volatile int m_is_writing;
+    volatile int    m_read_count;
+    volatile int    m_write_count;
+    volatile int    m_is_writing;
+    pthread_mutex_t m_write_mutex;
 
-    friend class spin_lock_read;
-    friend class spin_lock_write;
+    friend class fabs_spin_rwlock_read;
+    friend class fabs_spin_rwlock_write;
 };
 
-class spin_lock_read {
+class fabs_spin_rwlock_read {
 public:
-    spin_lock_read(spin_rwlock &lock) : m_lock(lock) {
+    fabs_spin_rwlock_read(fabs_spin_rwlock &lock) : m_lock(lock) {
         int wc = lock.m_write_count;
         int i = 0;
         while (lock.m_write_count > 0) {
@@ -36,13 +42,14 @@ public:
         }
 
         while (__sync_lock_test_and_set(&lock.m_is_writing, 1)) {
-            while (lock.m_is_writing) _MM_PAUSE(); // busy-wait
+            while (lock.m_is_writing) _MM_PAUSE; // busy-wait
         }
+
         __sync_fetch_and_add(&lock.m_read_count, 1);
         __sync_lock_release(&m_lock.m_is_writing);
     }
 
-    ~spin_lock_read() {
+    ~fabs_spin_rwlock_read() {
         unlock();
     }
 
@@ -55,12 +62,12 @@ public:
     }
 
 private:
-    spin_rwlock &m_lock;
+    fabs_spin_rwlock &m_lock;
 };
 
-class spin_lock_write {
+class fabs_spin_rwlock_write {
 public:
-    spin_lock_write(spin_rwlock &lock) : m_lock(lock) {
+    fabs_spin_rwlock_write(fabs_spin_rwlock &lock) : m_lock(lock) {
         __sync_fetch_and_add(&m_lock.m_write_count, 1);
         for (;;) {
             if (lock.m_read_count > 0)
@@ -78,7 +85,7 @@ public:
         }
     }
 
-    ~spin_lock_write() {
+    ~fabs_spin_rwlock_write() {
         unlock();
     }
 
@@ -88,7 +95,7 @@ public:
     }
 
 private:
-    spin_rwlock &m_lock;
+    fabs_spin_rwlock &m_lock;
 };
 
 #endif // FABS_SPIN_RWLOCK_HPP
